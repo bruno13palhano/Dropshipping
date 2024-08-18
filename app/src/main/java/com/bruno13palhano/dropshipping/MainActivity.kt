e3271a2070f50557b1aedbe7b6658c57db8083e6
bruno13palhano <bruno13palhano@gmail.com>
2024-08-18 20:16:48 -0300

    Renove unused code

--- app/src/main/java/com/bruno13palhano/dropshipping/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/dropshipping/MainActivity.kt
@@ -3,30 +3,22 @@ package com.bruno13palhano.dropshipping
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.bruno13palhano.dropshipping.ui.menu.BottomMenu
 import com.bruno13palhano.dropshipping.ui.navigation.MainNavGraph
 import com.bruno13palhano.dropshipping.ui.theme.DropshippingTheme
-import com.bruno13palhano.product.navigation.ProductsRoutes
-import com.bruno13palhano.product.navigation.productsScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContent {
             DropshippingTheme {
                 val navController = rememberNavController()
@@ -50,21 +42,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DropshippingTheme {
-        Greeting("Android")
     }
 }
