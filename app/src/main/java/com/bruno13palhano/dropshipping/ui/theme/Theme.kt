7f5ffb26d09853cbe5e6aa80cb0f68325391ef43
bruno13palhano <bruno13palhano@gmail.com>
2024-08-21 09:05:51 -0300

    Change statusBarColor and navigationBarColor

--- app/src/main/java/com/bruno13palhano/dropshipping/ui/theme/Theme.kt
+++ app/src/main/java/com/bruno13palhano/dropshipping/ui/theme/Theme.kt
@@ -8,8 +8,13 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -48,6 +53,15 @@ fun DropshippingTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.surfaceColorAtElevation(3.dp).toArgb()
+        }
     }
 
     MaterialTheme(
