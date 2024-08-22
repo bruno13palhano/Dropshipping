7cb7db00c21d5f1c4174344545ae652cb5522566
bruno13palhano <bruno13palhano@gmail.com>
2024-08-21 22:06:25 -0300

    Add common list item

---
+++ core/ui/src/main/java/com/bruno13palhano/ui/components/Items.kt
@@ -0,0 +1,40 @@
+package com.bruno13palhano.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+
+@Composable
+fun ElevatedListItem(
+    modifier: Modifier = Modifier,
+    onItemClick: () -> Unit,
+    onDeleteItemClick: () -> Unit,
+    headLineContent: @Composable () -> Unit
+) {
+    ElevatedCard(
+        modifier = modifier,
+        onClick = onItemClick
+    ) {
+        ListItem(
+            trailingContent = {
+                IconButton(
+                    modifier = Modifier.semantics { contentDescription = "Delete icon" },
+                    onClick = onDeleteItemClick
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Delete,
+                        contentDescription = null
+                    )
+                }
+            },
+            headlineContent = headLineContent
+        )
+    }
+}
