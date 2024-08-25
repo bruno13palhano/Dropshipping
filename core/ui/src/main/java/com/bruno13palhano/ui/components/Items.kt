0eebc95d3ae1e3daa4488b14c63dcc576d2ccc0d
bruno13palhano <bruno13palhano@gmail.com>
2024-08-25 13:27:05 -0300

    Add ExpandedListItem

--- core/ui/src/main/java/com/bruno13palhano/ui/components/Items.kt
+++ core/ui/src/main/java/com/bruno13palhano/ui/components/Items.kt
@@ -1,11 +1,19 @@
 package com.bruno13palhano.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -40,6 +48,33 @@ fun ElevatedListItem(
                 }
             },
             headlineContent = headlineContent
+        )
+    }
+}
+
+@Composable
+fun ExpandedListItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    shape: Shape,
+    headlineContent: @Composable (ColumnScope.() -> Unit)
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+
+    ElevatedCard(
+        modifier = modifier,
+        onClick = { isExpanded = !isExpanded },
+        shape = shape,
+    ) {
+        ListItem(
+            headlineContent = {
+                Column {
+                    Text(text = title)
+                    AnimatedVisibility (isExpanded) {
+                        Column { headlineContent() }
+                    }
+                }
+            }
         )
     }
 }
