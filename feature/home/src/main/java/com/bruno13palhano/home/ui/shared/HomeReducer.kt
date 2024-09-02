772c65d80ce053c456e72be7fa66f308069e1388
bruno13palhano <bruno13palhano@gmail.com>
2024-09-02 06:41:42 -0300

    Rename HomeState param from reduce method

--- feature/home/src/main/java/com/bruno13palhano/home/ui/shared/HomeReducer.kt
+++ feature/home/src/main/java/com/bruno13palhano/home/ui/shared/HomeReducer.kt
@@ -2,33 +2,33 @@ package com.bruno13palhano.home.ui.shared
 
 import com.bruno13palhano.ui.shared.Reducer
 
-class HomeReducer : Reducer<HomeState, HomeEvent, HomeEffect> {
+internal class HomeReducer : Reducer<HomeState, HomeEvent, HomeEffect> {
     override fun reduce(
-        state: HomeState,
+        previousState: HomeState,
         event: HomeEvent
     ): Pair<HomeState, HomeEffect?> {
         return when (event) {
             is HomeEvent.UpdateProfitVisibility -> {
-                state.copy(profitVisible = event.visible) to null
+                previousState.copy(profitVisible = event.visible) to null
             }
             is HomeEvent.UpdateProfit -> {
-                state.copy(profit = event.profit) to null
+                previousState.copy(profit = event.profit) to null
             }
             is HomeEvent.UpdateReceiptsVisibility -> {
-                state.copy(receiptsVisible = event.visible) to null
+                previousState.copy(receiptsVisible = event.visible) to null
             }
             is HomeEvent.UpdateExpandedItem -> {
-                state.copy(
-                    expandedItems = state.expandedItems.filter {
+                previousState.copy(
+                    expandedItems = previousState.expandedItems.filter {
                         it.first != event.expandedItem.first
                     }.plus(event.expandedItem)
                 ) to null
             }
             is HomeEvent.UpdateLastReceipts -> {
-                state.copy(lastReceipts = event.lastReceipts) to null
+                previousState.copy(lastReceipts = event.lastReceipts) to null
             }
             is HomeEvent.UpdateMostSale -> {
-                state.copy(mostSale = event.mostSale) to null
+                previousState.copy(mostSale = event.mostSale) to null
             }
         }
     }
