c77cb9f27d0ad2719252d7eb6d63c20f84b3de5b
bruno13palhano <bruno13palhano@gmail.com>
2024-09-02 20:15:23 -0300

    Add a Reducer implementation for Products

---
+++ feature/product/src/main/java/com/bruno13palhano/product/ui/shared/ProductsReducer.kt
@@ -0,0 +1,103 @@
+package com.bruno13palhano.product.ui.shared
+
+import com.bruno13palhano.ui.shared.Reducer
+
+internal class ProductsReducer : Reducer<ProductsState, ProductsEvent, ProductsEffect> {
+    override fun reduce(
+        previousState: ProductsState,
+        event: ProductsEvent
+    ): Pair<ProductsState, ProductsEffect?> {
+        return when (event) {
+            is ProductsEvent.ProductUpdating -> {
+                previousState.copy(
+                    isIdle = false,
+                    isProductUpdating = event.isUpdating,
+                    isProductAdding = false,
+                    isProductCanceling = false,
+                    isProductDeleting = false,
+                    isError = false,
+                    currentProduct = event.product,
+                ) to null
+            }
+            is ProductsEvent.ProductAdding -> {
+                previousState.copy(
+                    isIdle = false,
+                    isProductUpdating = false,
+                    isProductAdding = event.isAdding,
+                    isProductCanceling = false,
+                    isProductDeleting = false,
+                    isError = false
+                ) to null
+            }
+            is ProductsEvent.ProductCanceling -> {
+                previousState.copy(
+                    isIdle = false,
+                    isProductUpdating = false,
+                    isProductAdding = false,
+                    isProductCanceling = event.isCanceling,
+                    isProductDeleting = false,
+                    isError = false
+                ) to ProductsEffect.ShowCanceledMessage
+            }
+            is ProductsEvent.ProductDeleting -> {
+                previousState.copy(
+                    isIdle = false,
+                    isProductUpdating = false,
+                    isProductAdding = false,
+                    isProductCanceling = false,
+                    isProductDeleting = event.isDeleting,
+                    isError = false
+                ) to ProductsEffect.ShowDeletedMessage
+            }
+            is ProductsEvent.IdleState -> {
+                previousState.copy(
+                    isIdle = event.isIdle,
+                    isProductUpdating = false,
+                    isProductAdding = false,
+                    isProductCanceling = false,
+                    isProductDeleting = false,
+                    hasInvalidField = false,
+                    isError = false
+                ) to null
+            }
+            is ProductsEvent.ProductUpdatingInvalidField -> {
+                previousState.copy(
+                    isIdle = false,
+                    isProductUpdating = true,
+                    isProductAdding = false,
+                    isProductCanceling = false,
+                    isProductDeleting = false,
+                    hasInvalidField = event.hasInvalidField,
+                    isError = false
+                ) to ProductsEffect.ShowErrorMessage
+            }
+            is ProductsEvent.ProductAddingInvalidField -> {
+                previousState.copy(
+                    isIdle = false,
+                    isProductUpdating = false,
+                    isProductAdding = true,
+                    isProductCanceling = false,
+                    isProductDeleting = false,
+                    hasInvalidField = event.hasInvalidField,
+                    isError = false
+                ) to ProductsEffect.ShowErrorMessage
+            }
+            is ProductsEvent.ErrorState -> {
+                previousState.copy(
+                    isIdle = false,
+                    isProductUpdating = false,
+                    isProductAdding = false,
+                    isProductCanceling = false,
+                    isProductDeleting = false,
+                    isError = event.isError
+                ) to ProductsEffect.ShowErrorMessage
+            }
+            is ProductsEvent.UpdateProducts -> {
+                previousState.copy(
+                    isProductsUpdating = event.isUpdating,
+                    products = event.products
+                ) to null
+            }
+        }
+    }
+}
