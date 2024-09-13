b880760d25c42da389479e82fb9715c892c41a8e
bruno13palhano <bruno13palhano@gmail.com>
2024-09-13 08:25:08 -0300

    Split and rename state, event and effect implementations for product

--- feature/product/src/main/java/com/bruno13palhano/product/ui/shared/ProductModel.kt
+++ feature/product/src/main/java/com/bruno13palhano/product/ui/shared/ProductModel.kt
@@ -2,44 +2,7 @@ package com.bruno13palhano.product.ui.shared
 
 import androidx.compose.runtime.Immutable
 import com.bruno13palhano.model.Product
-import com.bruno13palhano.ui.components.CommonItem
 import com.bruno13palhano.ui.shared.Reducer
-
-@Immutable
-internal data class ProductsState(
-    val productsLoading: Boolean,
-    val editProduct: Boolean,
-    val addProduct: Boolean,
-    val deleteProduct: Boolean,
-    val products: List<CommonItem>
-) : Reducer.ViewState {
-    companion object {
-        val INITIAL_STATE = ProductsState(
-            productsLoading = true,
-            editProduct = false,
-            addProduct = false,
-            deleteProduct = false,
-            products = emptyList()
-        )
-    }
-}
-
-@Immutable
-internal sealed interface ProductsEvent : Reducer.ViewEvent {
-    data class UpdateProducts(val isLoading: Boolean, val products: List<CommonItem>) : ProductsEvent
-    data class EditProduct(val editProduct: Boolean, val id: Long) : ProductsEvent
-    data class AddProduct(val addProduct: Boolean) : ProductsEvent
-    data class UpdateDeletingProduct(val isDeleting: Boolean, val id: Long) : ProductsEvent
-    data object OnDeleteProductSuccessfully : ProductsEvent
-}
-
-@Immutable
-internal sealed interface ProductsEffect : Reducer.ViewEffect {
-    data class NavigateToEditProduct(val id: Long) : ProductsEffect
-    data object NavigateToAddProduct : ProductsEffect
-    data class DeleteProduct(val id: Long) : ProductsEffect
-    data object ShowDeletedMessage : ProductsEffect
-}
 
 @Immutable
 internal data class ProductState(
