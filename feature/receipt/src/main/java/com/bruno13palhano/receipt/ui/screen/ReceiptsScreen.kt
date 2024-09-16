b0268b4a1e680bf9f1c98f98937ae98f4076ec79
bruno13palhano <bruno13palhano@gmail.com>
2024-09-16 20:59:49 -0300

    Refactor ReceiptsScreen

--- feature/receipt/src/main/java/com/bruno13palhano/receipt/ui/screen/ReceiptsScreen.kt
+++ feature/receipt/src/main/java/com/bruno13palhano/receipt/ui/screen/ReceiptsScreen.kt
@@ -1,45 +1,47 @@
 package com.bruno13palhano.receipt.ui.screen
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.bruno13palhano.model.Product
+import com.bruno13palhano.model.Receipt
 import com.bruno13palhano.receipt.R
 import com.bruno13palhano.receipt.ui.shared.ReceiptsEffect
 import com.bruno13palhano.receipt.ui.shared.ReceiptsEvent
 import com.bruno13palhano.receipt.ui.viewmodel.ReceiptsViewModel
-import com.bruno13palhano.ui.components.CommonItem
-import com.bruno13palhano.ui.components.ElevatedListItem
+import com.bruno13palhano.ui.components.dateFormat
 import com.bruno13palhano.ui.components.rememberFlowWithLifecycle
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.flowOf
 
 @Composable
 internal fun ReceiptsRoute(
@@ -51,11 +53,8 @@ internal fun ReceiptsRoute(
     LaunchedEffect(key1 = Unit) { viewModel.getReceipts() }
 
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val receipts = state.receipts.collectAsLazyPagingItems()
     val effect = rememberFlowWithLifecycle(flow = viewModel.effect)
-    val snackbarHostState = remember { SnackbarHostState() }
-    val scope = rememberCoroutineScope()
-
-    val deleteMessage = stringResource(id = R.string.delete_receipt_message)
 
     LaunchedEffect(effect) {
         effect.collect { action ->
@@ -63,19 +62,9 @@ internal fun ReceiptsRoute(
                 is ReceiptsEffect.NavigateToEditReceipt -> {
                     onItemClick(action.id)
                 }
+
                 is ReceiptsEffect.NavigateToSearchProduct -> {
                     onAddNewReceiptClick()
-                }
-                is ReceiptsEffect.DeleteReceipt -> {
-                    viewModel.deleteReceipt(id = action.id)
-                }
-                is ReceiptsEffect.ShowDeletedMessage -> {
-                    scope.launch {
-                        snackbarHostState.showSnackbar(
-                            message = deleteMessage,
-                            withDismissAction = true
-                        )
-                    }
                 }
             }
         }
@@ -83,16 +72,10 @@ internal fun ReceiptsRoute(
 
     ReceiptsContent(
         modifier = modifier,
-        snackbarHostState = snackbarHostState,
-        receipts = state.receipts,
+        receipts = receipts,
         onReceiptItemClick = { id ->
             viewModel.sendEvent(
                 event = ReceiptsEvent.EditReceipt(editReceipt = true, id = id)
-            )
-        },
-        onDeleteReceiptClick = { id ->
-            viewModel.sendEvent(
-                event = ReceiptsEvent.UpdateDeletingReceipt(isDeleting = true, id = id)
             )
         },
         onAddNewReceiptClick = {
@@ -105,10 +88,8 @@ internal fun ReceiptsRoute(
 @Composable
 internal fun ReceiptsContent(
     modifier: Modifier = Modifier,
-    snackbarHostState: SnackbarHostState,
-    receipts: List<CommonItem>,
+    receipts: LazyPagingItems<Receipt>,
     onReceiptItemClick: (id: Long) -> Unit,
-    onDeleteReceiptClick: (id: Long) -> Unit,
     onAddNewReceiptClick: () -> Unit
 ) {
     Scaffold(
@@ -117,7 +98,6 @@ internal fun ReceiptsContent(
             .consumeWindowInsets(WindowInsets.safeDrawing)
             .semantics { contentDescription = "Receipts content" },
         topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.receipts)) }) },
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         floatingActionButton = {
             FloatingActionButton(onClick = onAddNewReceiptClick) {
                 Icon(
@@ -131,20 +111,48 @@ internal fun ReceiptsContent(
             modifier = Modifier
                 .semantics { contentDescription = "List of receipts" }
                 .consumeWindowInsets(it),
-            contentPadding = it
+            contentPadding = PaddingValues(
+                vertical = 4.dp + it.calculateTopPadding(),
+                horizontal = 4.dp + it.calculateStartPadding(LayoutDirection.Ltr),
+            )
         ) {
-            items(items = receipts, key = { receipt -> receipt.id }) { receipt ->
-                ElevatedListItem(
-                    modifier = Modifier.padding(4.dp),
-                    icon = Icons.Filled.Delete,
-                    iconDescription = stringResource(id = R.string.delete),
-                    onItemClick = { onReceiptItemClick(receipt.id) },
-                    onIconClick = { onDeleteReceiptClick(receipt.id) }
-                ) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = receipt.title
-                    )
+            items(count = receipts.itemCount) { index ->
+                receipts[index]?.let { receipt ->
+                    ElevatedCard(
+                        modifier = Modifier.padding(4.dp),
+                        onClick = { onReceiptItemClick(receipt.id) }
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 4.dp)
+                                .fillMaxWidth(),
+                            text = receipt.customerName
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(horizontal = 8.dp, vertical = 4.dp)
+                                .fillMaxWidth(),
+                            text = receipt.product.name
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(horizontal = 8.dp, vertical = 4.dp)
+                                .fillMaxWidth(),
+                            text = stringResource(
+                                id = R.string.units_sold_info,
+                                receipt.quantity
+                            )
+                        )
+                        Text(
+                            modifier = Modifier
+                                .padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 8.dp)
+                                .fillMaxWidth(),
+                            text = stringResource(
+                                id = R.string.data_info,
+                                dateFormat.format(receipt.requestDate)
+                            )
+                        )
+                    }
                 }
             }
         }
@@ -155,17 +163,39 @@ internal fun ReceiptsContent(
 @Composable
 private fun ReceiptsContentPreview() {
     ReceiptsContent(
-        snackbarHostState = remember { SnackbarHostState() },
-        receipts = listOf(
-            CommonItem(id = 1, title = "receipt 1"),
-            CommonItem(id = 2, title = "receipt 2"),
-            CommonItem(id = 3, title = "receipt 3"),
-            CommonItem(id = 4, title = "receipt 4"),
-            CommonItem(id = 5, title = "receipt 5"),
-            CommonItem(id = 6, title = "receipt 6"),
-        ),
+        receipts = flowOf(
+            PagingData.from(
+                listOf(
+                    Receipt(
+                        id = 1L,
+                        product = Product(1L, "12","Homem"),
+                        requestNumber = 1,
+                        requestDate = 1719999234567L,
+                        customerName = "First Customer",
+                        quantity = 1,
+                        naturaPrice = 56.99f,
+                        amazonPrice = 88.9f,
+                        paymentOption = "cash",
+                        canceled = false,
+                        observations = ""
+                    ),
+                    Receipt(
+                        id = 2L,
+                        product = Product(2L, "12","Essencial"),
+                        requestNumber = 1,
+                        requestDate = 1719999234567L,
+                        customerName = "Second Customer",
+                        quantity = 2,
+                        naturaPrice = 56.99f,
+                        amazonPrice = 88.9f,
+                        paymentOption = "cash",
+                        canceled = false,
+                        observations = ""
+                    )
+                )
+            )
+        ).collectAsLazyPagingItems(),
         onReceiptItemClick = {},
-        onDeleteReceiptClick = {},
         onAddNewReceiptClick = {}
     )
 }
