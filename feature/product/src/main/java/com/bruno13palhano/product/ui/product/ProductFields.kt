6a80053a544213a53ad352f084d9812f71dfff7b
bruno13palhano <bruno13palhano@gmail.com>
2024-09-26 11:19:22 -0300

    Move setFields, isValid and toProduct methods to ProdutFields

--- feature/product/src/main/java/com/bruno13palhano/product/ui/product/ProductFields.kt
+++ feature/product/src/main/java/com/bruno13palhano/product/ui/product/ProductFields.kt
@@ -1,8 +1,9 @@
-package com.bruno13palhano.product.ui.viewmodel
+package com.bruno13palhano.product.ui.product
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.bruno13palhano.model.Product
 import javax.inject.Inject
 
 /**
@@ -17,4 +18,21 @@ internal class ProductFields @Inject constructor() {
     fun updateNaturaCode(naturaCode: String) { this.naturaCode = naturaCode }
 
     fun updateProductName(productName: String) { this.productName = productName }
+
+    fun isValid(): Boolean {
+        return naturaCode.isNotBlank() && productName.isNotBlank()
+    }
+
+    fun toProduct(id: Long = 0L): Product {
+        return Product(
+            id = id,
+            naturaCode = naturaCode,
+            name = productName
+        )
+    }
+
+    fun setFields(product: Product) {
+        updateNaturaCode(naturaCode = product.naturaCode)
+        updateProductName(productName = product.name)
+    }
 }
